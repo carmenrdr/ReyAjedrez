a943f5862c1a4aff0209209c2109c67445d3cc54
carudelro <carmenrumi@gmail.com>
2023-11-25 22:03:04 +0100

    Clase Rey con sus atributos.

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public class Rey {
+
+    private Color colorRey;
+    private Posicion posicionRey;
+    private int totalMovimientos;
+
+}
