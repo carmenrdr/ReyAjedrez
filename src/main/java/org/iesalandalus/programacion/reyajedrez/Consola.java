962cb3fff20da49cef8cbe9c67bf9a8111d27f29
carudelro <carmenrumi@gmail.com>
2023-11-26 14:17:43 +0100

    Clase Consola, mostrarMenu.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -2,4 +2,14 @@ package org.iesalandalus.programacion.reyajedrez;
 
 public class Consola {
 
+    private Consola(){}
+
+    public static void mostrarMenu() {
+        System.out.print("Las opciones son las siguientes; \n" +
+                "1. Crea un rey por defecto.\n" +
+                "2. Crea un rey eligiendo el color.\n" +
+                "3. Mover pieza.\n" +
+                "4. Salir del juego.");
+    }
+
 }
