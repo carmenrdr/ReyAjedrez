0b4f95fc78fe0e8fd2689c40ae860bc9a0c8a640
carudelro <carmenrumi@gmail.com>
2023-11-26 14:45:50 +0100

    Clase Consola, elegirOpcionMenu.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -1,15 +1,27 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import java.util.Scanner;
+
 public class Consola {
 
     private Consola(){}
 
     public static void mostrarMenu() {
-        System.out.print("Las opciones son las siguientes; \n" +
+        System.out.println("Las opciones son las siguientes: \n" +
                 "1. Crea un rey por defecto.\n" +
                 "2. Crea un rey eligiendo el color.\n" +
                 "3. Mover pieza.\n" +
                 "4. Salir del juego.");
+    }
+
+    public static void elegirOpcionMenu() {
+        Scanner opcion = new Scanner(System.in);
+        int eleccion;
+        do {
+            System.out.print("Elija una opción: ");
+            eleccion = opcion.nextInt();
+        } while (eleccion < 1 || eleccion > 4);
+        System.out.println("La opción elegida es: " + eleccion + ".");
     }
 
 }
