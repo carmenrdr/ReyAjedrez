0cd0759e345e5cbc38822004aec4df148403603c
carudelro <carmenrumi@gmail.com>
2023-11-26 15:37:16 +0100

    Clase main, crear ejecutarOpcion.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,21 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
+
 public class MainApp {
     public static void main(String[] args) {
-
     }
+
+    public Rey nuestroRey;
+
+    private void ejecutarOpcion(int eleccion){
+        switch (eleccion){
+            case 1 -> ;
+            case 2 -> ;
+            case 3 -> ;
+            case 4 -> ;
+        }
+    }
+
+
 }
